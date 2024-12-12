e89d445d471fbcfb8c40752404e0379fb6f716ab
ekerney <kerneyneels@gmail.com>
2024-12-11 20:53:41 -0500

    remove yuge test geojson data

--- lua/geovim/test.lua
+++ lua/geovim/test.lua
@@ -34,16 +34,16 @@ print(a["x"])       --> 11
 
 
 a = {} -- empty table
-create 1000 new entries
+-- create 1000 new entries
 for i = 1, 1000 do a[i] = i * 2 end
-print(a[9]) --> 18
+print(a[9])   --> 18
 a["x"] = 10
 print(a["x"]) --> 10
 print(a["y"]) --> nil
 
-a.x = 10   -- same as a["x"] = 10
-print(a.x) -- same as print(a["x"])
-print(a.y) -- same as print(a["y"])
+a.x = 10      -- same as a["x"] = 10
+print(a.x)    -- same as print(a["x"])
+print(a.y)    -- same as print(a["y"])
 
 a = {}; a.x = 1; a.y = 0
 b = {}; b.x = 1; b.y = 0
