0b14424cf13b5cfb12aef50f791f251f4eb2ada0
ekerney <kerneyneels@gmail.com>
2024-12-16 21:11:06 -0500

    slow progress

--- lua/geovim/geovim.lua
+++ lua/geovim/geovim.lua
@@ -3,7 +3,8 @@ local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
 local previewers = require('telescope.previewers')
-local utils = require('telescope.previewers.utils')
+-- local utils = require('telescope.previewers.utils')
+local M = {}
 
 local log = require('plenary.log'):new()
 log.level = 'debug'
@@ -11,40 +12,24 @@ log.level = 'debug'
 -- automatically source file on <leader>w  save
 vim.keymap.set("n", "<leader>w", ":w<cr>:source %<cr>")
 
-local M = {}
 
 local find_geojson = function(opts)
   opts = opts or {}
   opts.cwd = opts.cwd or vim.uv.cwd()
 
-  -- finders.new_async_job {
-  --   command_generator = function(prompt)
-  --     if not prompt or prompt == "" then
-  --       return nil
-  --     end
-  --
-  --     local pieces = vim.split(prompt, "  ")
-  --     local args =
-  --   end
-  -- }
+  local finder = finders.new_async_job({
+    command_generator = function()
+      return { "rg", "-g", "*.geojson", "--files" }
+    end,
+  })
 
   pickers.new(opts, {
-    finder = finders.new_async_job({
-      command_generator = function()
-        return { "rg", "-g", "*.geojson", "--files" }
-      end,
-      entry_maker = function(entry)
-        return {
-          value = entry,
-          display = entry,
-          ordinal = entry,
-        }
-      end
-    }),
-
-    sorter = conf.generic_sorter(opts),
-    previewer = conf.grep_previewer(opts, {}),
-
+    debounce = 100,
+    prompt_title = "(:)GeoVim(:)",
+    log.debug("OPTS", opts),
+    finder = finder,
+    previewer = conf.grep_previewer(opts),
+    -- sorter = conf.generic_sorter(opts),
 
   }):find()
 end
@@ -52,6 +37,6 @@ end
 M.setup = function()
   vim.keymap.set("n", "<leader>gv", find_geojson)
 end
-
+print("HEY")
 
 return M
